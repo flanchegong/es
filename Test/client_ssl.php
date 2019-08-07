57ea9e72797f9fdae8f3f89fea14bf4abbf57aea
flanche <flanchegong@163.com>
2019-08-07 13:35:00 +0800

    test

--- Test/client_ssl.php
+++ Test/client_ssl.php
@@ -1,6 +1,6 @@
 <?php
 $client = new swoole_client(SWOOLE_SOCK_TCP);
-if (!$client->connect('127.0.0.1', 9501, -1))
+if (!$client->connect('127.0.0.1', 9503, -1))
 {
     exit("connect failed. Error: {$client->errCode}\n");
 }
