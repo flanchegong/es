9d167e882b7dcee6f2c80f72a9ca4bfd38063197
flanche <flanchegong@163.com>
2019-08-08 16:42:23 +0800

    getaddrinfo

--- Test/gethostbyname.php
+++ Test/gethostbyname.php
@@ -7,4 +7,6 @@ go(function(){
     echo $ip."\n";
     $ip = swoole_async_dns_lookup_coro("www.baidu.com");
     echo $ip."\n";
+    $array = co::getaddrinfo("www.baidu.com");
+    var_dump($array);
 });
