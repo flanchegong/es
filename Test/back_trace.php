f4bad8a91e6665a6822d44d3a18e07580856a9b3
flanche <flanchegong@163.com>
2019-08-08 14:43:47 +0800

    Coroutine::getBackTrace

---
+++ Test/back_trace.php
@@ -0,0 +1,24 @@
+<?php
+function test1() {
+    test2();
+}
+
+function test2() {
+    while(true) {
+        co::sleep(10);
+        echo __FUNCTION__." \n";
+    }
+}
+
+$cid = go(function () {
+    test1();
+});
+
+go(function () use ($cid) {
+    while(true) {
+        echo "BackTrace[$cid]:\n-----------------------------------------------\n";
+        //返回数组，需要自行格式化输出
+        var_dump(co::getBackTrace($cid))."\n";
+        co::sleep(3);
+    }
+});
