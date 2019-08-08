0ba28a9db928fda24af4abf83c40d15afe46edc4
flanche <flanchegong@163.com>
2019-08-08 09:52:51 +0800

    go

---
+++ Test/go.php
@@ -0,0 +1,15 @@
+<?php
+go(function () {
+    go(function () {
+        go(function () {
+            Co::sleep(0.001);
+            var_dump(Co::exists(Co::getPcid())); // 1: true
+        });
+        go(function () {
+            Co::sleep(0.003);
+            var_dump(Co::exists(Co::getPcid())); // 3: false
+        });
+        Co::sleep(0.002);
+        var_dump(Co::exists(Co::getPcid())); // 2: false
+    });
+});
