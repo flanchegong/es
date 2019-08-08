6d95818ab19720fb596e57bd332e89279089a919
flanche <flanchegong@163.com>
2019-08-08 16:14:54 +0800

    test

--- Test/fwrite.php
+++ Test/fwrite.php
@@ -2,7 +2,7 @@
 namespace Flanche\Test;
 
 use Swoole\Coroutine as co;
-$fp = fopen(__DIR__ . "/test.data", "a+");
+$fp = fopen(__DIR__ . "/test.txt", "a+");
 co::create(function () use ($fp)
 {
     $r =  co::fwrite($fp, "hello world\n", 5);
