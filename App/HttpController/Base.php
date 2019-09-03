2b702ea6fe8b2d441342fa5e300610ac7c83033a
flanche <flanchegong@163.com>
2019-09-03 19:49:17 +0800

    device

--- App/HttpController/Base.php
+++ App/HttpController/Base.php
@@ -4,6 +4,8 @@ namespace App\HttpController;
 use EasySwoole\Http\AbstractInterface\Controller;
 use EasySwoole\Core\Http\Request;
 use EasySwoole\Core\Http\Response;
+use EasySwoole\Http\Message\Status;
+use EasySwoole\EasySwoole\Trigger;
 class Base extends Controller
 {
 
@@ -15,12 +17,31 @@ class Base extends Controller
 
     function index()
     {
+        $this->actionNotFound('index');
         // TODO: Implement index() method.
     }
 
     function header()
     {
         $this->response()->withHeader('Content-type', 'text/html;charset=utf-8');
+    }
+
+    protected function onRequest(?string $action): ?bool
+    {
+        //模拟拦截
+        //当没有传code的时候则拦截
+        if (empty($this->request()->getRequestParam('code'))) {
+            $this->writeJson(Status::CODE_BAD_REQUEST, ['errorCode' => 1, 'data' => []], 'code不存在');
+            return false;
+        }
+        return true;
+    }
+
+    protected function onException(\Throwable $throwable): void
+    {
+        //拦截错误进日志,使控制器继续运行
+        Trigger::getInstance()->throwable($throwable);
+        $this->writeJson(Status::CODE_INTERNAL_SERVER_ERROR, null, $throwable->getMessage());
     }
 
 }
--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -25,6 +25,9 @@ use EasySwoole\Trace\TrackerManager;
 use App\Process\Test;
 use EasySwoole\Component\Process\Config as p_config;
 use EasySwoole\EasySwoole\Crontab\Crontab;
+use App\Device\Command;
+use App\Device\DeviceActor;
+use App\Device\DeviceManager;
 class EasySwooleEvent implements Event
 {
 
@@ -111,6 +114,9 @@ class EasySwooleEvent implements Event
 
         //注册定时任务
         self::registerCrontabTask();
+
+        //注册Actor
+        self::registerDevice($register);
     }
 
     public static function onRequest(Request $request, Response $response): bool
@@ -171,5 +177,51 @@ class EasySwooleEvent implements Event
             }
             $Crontab->addTask($task['class']);
         }
+    }
+
+    private static function registerDevice($register){
+        //注册Actor
+        Actor::getInstance()->register(DeviceActor::class);
+        Actor::getInstance()->setListenPort(9600)
+            ->setTrigger(Trigger::getInstance())
+            ->setListenAddress('0.0.0.0')
+            ->setTempDir(EASYSWOOLE_TEMP_DIR);
+        Actor::getInstance()->attachServer(ServerManager::getInstance()->getSwooleServer());
+        //创建Table用来记录 fd与actor的映射关系
+        DeviceManager::tableInit();
+        $register->add($register::onOpen,function (\swoole_websocket_server $svr, \swoole_http_request $req){
+            if(!isset($req->get['deviceId'])){
+                ServerManager::getInstance()->getSwooleServer()->push($req->fd,'deviceId@length=8 is require');
+                ServerManager::getInstance()->getSwooleServer()->close($req->fd);
+                return;
+            }
+            $deviceId = $req->get['deviceId'];
+            $info = DeviceManager::deviceInfo($deviceId);
+            if($info){
+                //说明是断线重连
+                $command = new Command();
+                $command->setCommand($command::RECONNECT);
+                $command->setArg($req->fd);
+                DeviceActor::client()->send($info->getActorId(),$command);
+            }else{
+                //第一次链接服务端
+                DeviceActor::client()->create([
+                    'deviceId'=>$deviceId,
+                    'fd'=>$req->fd
+                ]);
+            }
+        });
+
+        $register->add($register::onMessage,function (\swoole_websocket_server  $server, \swoole_websocket_frame $frame){
+            $info = DeviceManager::deviceInfoByFd($frame->fd);
+            if($info){
+                $com = new Command();
+                $com->setCommand($com::WS_MSG);
+                $com->setArg($frame->data);
+                DeviceActor::client()->send($info->getActorId(),$com);
+            }else{
+                $server->close($frame->fd);
+            }
+        });
     }
 }
